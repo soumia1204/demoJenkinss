bfb3fe88343fbd8363452992c8b7e415cbdab459
Soumia Bata <83792984+soumia1204@users.noreply.github.com>
2024-02-08 15:41:51 +0100

    Update PersonService.java

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -46,6 +46,13 @@ public class PersonService {
 		p.setFirstName("webhook");
 		p.setLastName("testJenjinsJob");
 		persons.add(p);
+
+		p = new Person();
+		p.setId("6");
+		p.setAge(26);
+		p.setFirstName("Sophia");
+		p.setLastName("Sophia");
+		persons.add(p);
 		
 	}
 	public Person getPerson(String id) {
